5511390f698165169e11391312ddf244a9c7fc32
Reed Essick <reed.essick@gmail.com>
2024-12-16 17:58:41 -0500

    work to get plotting script in a more easily useable state

--- w4t/haar.py
+++ w4t/haar.py
@@ -9,10 +9,12 @@ import copy
 
 #-------------------------------------------------
 
+__SCALE__ = 1./2**0.5 # used to normalize sums
+
 def haar(array, axis=None):
     """take the Haar decomposition of array along axis. If axis is None, use the last axis of array (axis=-1)
-    approx = array[i+1] + array[i]
-    detail = array[i+1] - array[i]
+    approx = SCALE * (array[i+1] + array[i])
+    detail = SCALE * (array[i+1] - array[i])
     return approx, detail
     """
     if axis is None:
@@ -30,14 +32,14 @@ def haar(array, axis=None):
     jnds = np.take(array, jnds, axis=axis)
 
     # return
-    return jnds+inds, jnds-inds
+    return __SCALE__*(jnds+inds), __SCALE__*(jnds-inds) ### FIXME: include a normalization to keep stdv of Gaussian coeffs constant?
 
 #---
 
 def ihaar(approx, detail, axis=None):
     """invert the Haar decomposition along an axis. If axis is None, use the last axis of the array (axis=-1)
-    array[i+1] = 0.5*(approx + detail)
-    array[i]   = 0.5*(approx - detail)
+    array[i]   = 0.5 * (approx - detail) / SCALE
+    array[i+1] = 0.5 * (approx + detail) / SCALE
     return array
     """
     if axis is None:
@@ -58,8 +60,8 @@ def ihaar(approx, detail, axis=None):
     jnds = inds + 1
 
     shape = tuple(num if a==axis else 1 for a in range(len(shape)))
-    np.put_along_axis(array, inds.reshape(shape), 0.5*(approx-detail), axis)
-    np.put_along_axis(array, jnds.reshape(shape), 0.5*(approx+detail), axis)
+    np.put_along_axis(array, inds.reshape(shape), 0.5*(approx-detail)/__SCALE__, axis)
+    np.put_along_axis(array, jnds.reshape(shape), 0.5*(approx+detail)/__SCALE__, axis)
 
     # return
     return array

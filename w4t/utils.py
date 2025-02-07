6e58a265c3b2f1dc7b8ef1420e9a2670e113e00a
Reed Essick <reed.essick@gmail.com>
2025-02-07 15:20:04 -0500

    added another plot

--- w4t/utils.py
+++ w4t/utils.py
@@ -90,6 +90,208 @@ def load(fields, path=None, num_grid=DEFAULT_NUM_GRID, num_dim=DEFAULT_NUM_DIM, 
 
 #-------------------------------------------------
 
+def load_flash(path, fields):
+    """load data from a Flash simulation
+    """
+    raise NotImplementedError('''
+        # simulation attributes
+        self.filename           = filename
+        self.reformat           = reformat
+        self.sim_data_type      = sim_data_type
+        self.n_cores            = 0
+        self.nxb                = 0
+        self.nyb                = 0
+        self.nzb                = 0
+        self.n_cells            = 0
+        self.int_properties     = {}
+        self.str_properties     = {}
+        self.logic_properties   = {}
+
+        # read in the simulation properties
+        if self.sim_data_type == "flash":
+            self.__read_sim_properties()
+
+        # grid data attributes
+        # if the data is going to be reformated, preallocate the 3D
+        # arrays for the grid data
+        if self.reformat:
+            init_field = np.zeros((self.nyb*self.int_properties["jprocs"],
+                                   self.nxb*self.int_properties["iprocs"],
+                                   self.nzb*self.int_properties["kprocs"]), dtype=np.float32)
+        else:
+            # otherwise, preallocate the 1D arrays for the grid data
+            init_field = np.zeros(self.n_cells, dtype=np.float32)
+
+        # add a new method here for initialisation 
+        # (could speed up IO a bit but haven't properly timed it)
+
+        # read in state (read in true or false if the reader
+        # has actually been called -- this is all to save time 
+        # with derived vars. Note that the way that it is 
+        # written means that it will create new read_... state variables
+        # even if one hasn't been initialised here.)
+        self.read_dens          = False
+        self.read_vel           = False
+        self.read_mag           = False
+        self.derived_cur        = False
+
+        # use a CGS unit system
+        self.mu0                = 4 * np.pi
+
+    def __read_sim_properties(self) -> None:
+        """
+        This function reads in the FLASH field properties directly from
+        the hdf5 file metadata. 
+        
+        Author: James Beattie
+        
+        """
+        g = File(self.filename, 'r')
+        self.int_scalars        = {str(key).split("'")[1].strip(): value for key, value in g["integer scalars"]}
+        self.int_properties     = {str(key).split("'")[1].strip(): value for key, value in g["integer runtime parameters"]}
+        self.str_properties     = {str(key).split("'")[1].strip(): str(value).split("'")[1].strip() for key, value in g["string runtime parameters"]}
+        self.logic_properties   = {str(key).split("'")[1].strip(): value for key, value in g["logical runtime parameters"]}
+        g.close()
+
+        # read out properties of the grid
+        self.n_cores  = self.int_scalars["globalnumblocks"]
+        self.nxb      = self.int_scalars["nxb"]
+        self.nyb      = self.int_scalars["nyb"]
+        self.nzb      = self.int_scalars["nzb"]
+        self.iprocs   = self.int_properties["iprocs"]
+        self.jprocs   = self.int_properties["jprocs"]
+        self.kprocs   = self.int_properties["kprocs"]
+        self.n_cells  = self.n_cores*self.nxb*self.nyb*self.nzb
+        self.plot_file_num = self.int_scalars["plotfilenumber"]
+
+
+    def read(self,
+             field_str          : str,
+             vector_magnitude   : bool = False,
+             debug              : bool = False,
+             interpolate        : bool = True,
+             N_grid_x           : int  = 256,
+             N_grid_y           : int  = 256,
+             N_grid_z           : int  = 256,
+             verbose = False,
+        ) -> None:
+        """
+        This function reads in grid data.
+        
+        Args:
+            field_str (str):            The field to read in.
+            vector_magnitude (bool):    Whether to read in the magnitude of the vector field.
+            debug (bool):               Whether to print debug information.
+            N_grid_x (int):             The number of grid points to interpolate onto in the x direction.
+            N_grid_y (int):             The number of grid points to interpolate onto in the y direction.
+            N_grid_z (int):             The number of grid points to interpolate onto in the z direction.
+        
+        """
+
+        setattr(self,f"read_{field_str}",True)
+
+        if self.sim_data_type == "flash":
+            # setting the read in states here (this adds an attribute)
+            if field_lookup_type[field_str] == "scalar":
+                g = File(self.filename, 'r')
+                if verbose:
+                    print(f"Reading in grid attribute: {field_str}")
+
+                if self.reformat:
+                    if verbose:
+                        print(f"Reading in reformatted grid attribute: {field_str}")
+
+                    setattr(self, field_str,
+                            np.array([reformat_FLASH_field(g[field_str][:,:,:,:],
+                                                 self.nxb,
+                                                 self.nyb,
+                                                 self.nzb,
+                                                 self.iprocs,
+                                                 self.jprocs,
+                                                 self.kprocs,
+                                                 debug)]))
+                else:
+                    setattr(self, field_str, np.array([g[field_str][:,:,:,:]]))
+                g.close()
+
+
+def reformat_FLASH_field(field  : np.ndarray,
+                         nxb    : int,
+                         nyb    : int,
+                         nzb    : int,
+                         iprocs : int,
+                         jprocs : int,
+                         kprocs : int,
+                         debug  : bool) -> np.ndarray:
+    """
+    This function reformats the FLASH block / core format into
+    (x,y,z) format for processing in real-space coordinates utilising
+    numba's jit compiler, producing roughly a two-orders of magnitude
+    speedup compared to the pure python version.
+
+    INPUTS:
+    field   - the FLASH field in (core,block_x,block_y,block_z) coordinates
+    iprocs  - the number of cores in the x-direction
+    jprocs  - the number of cores in the y-direction
+    kprocs  - the number of cores in the z-direction
+    debug   - flag to print debug information
+
+
+    OUTPUTs:
+    field_sorted - the organised 3D field in (x,y,z) coordinates
+
+    """
+
+    if debug:
+        print(f"reformat_FLASH_field: nxb = {nxb}")
+        print(f"reformat_FLASH_field: nyb = {nyb}")
+        print(f"reformat_FLASH_field: nzb = {nzb}")
+        print(f"reformat_FLASH_field: iprocs = {iprocs}")
+        print(f"reformat_FLASH_field: jprocs = {jprocs}")
+        print(f"reformat_FLASH_field: kprocs = {kprocs}")
+
+    # Initialise an empty (x,y,z) field
+    # has to be the same dtype as input field (single precision)
+    # swap axes to get the correct orientation
+    # x = 0, y = 1, z = 2
+    return np.transpose(sort_flash_field(field,
+                                         nxb,
+                                         nyb,
+                                         nzb,
+                                         iprocs,
+                                         jprocs,
+                                         kprocs),
+                        (2,1,0))
+
+def sort_flash_field(field    : np.ndarray,
+                     nxb      : int,
+                     nyb      : int,
+                     nzb      : int,
+                     iprocs   : int,
+                     jprocs   : int,
+                     kprocs   : int) -> np.ndarray:
+
+    # Initialise an empty (x,y,z) field
+    field_sorted = np.zeros((nyb*jprocs,
+                             nzb*kprocs,
+                             nxb*iprocs),
+                            dtype=np.float32)
+
+    # The block counter for looping through blocks
+    block_counter = 0
+
+    # Sort the unsorted field
+    for j in range(jprocs):
+        for k in range(kprocs):
+            for i in range(iprocs):
+                field_sorted[j*nyb:(j+1)*nyb, k*nzb:(k+1)*nzb, i*nxb:(i+1)*nxb] = field[block_counter, :, :, :]
+                block_counter += 1
+    return field_sorted
+
+''')
+
+#-------------------------------------------------
+
 def moments(samples, index):
     """estimate moments of samples for each value in index (which should be an iterable). For example, index=[1,2] will compute the 1st and second moment of samples. Also estimates the covariance matrix between the estimators for the requested moments.
     """

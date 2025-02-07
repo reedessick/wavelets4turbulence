bb0ed1aee3c96b4fcfb541c3f18ada01965ab017
Reed Essick <essick@mussel.cita.utoronto.ca>
2025-02-07 14:59:30 -0500

    refactored a bit to start adding executables

--- env.sh
+++ env.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export PATH="$PWD/opt/bin:$PATH"
+
 export PYTHONPATH="$PWD/opt:$PYTHONPATH"
-export PYTHONPATH="$PWD/opt/local/lib/python3.10/dist-packages/:$PYTHONPATH"
+
+export PYTHONPATH="$PWD/opt/lib/python3.10/site-packages/:$PYTHONPATH"
+#export PYTHONPATH="$PWD/opt/local/lib/python3.10/dist-packages/:$PYTHONPATH"
--- w4t/utils.py
+++ w4t/utils.py
@@ -7,6 +7,87 @@ __author__ = "Reed Essick (reed.essick@gmail.com)"
 import numpy as np
 from scipy.special import comb # comb(n, k) = "n choose k" = n! / ((n-k)! k!)
 
+### non-standard libraries
+try:
+    from PLASMAtools.read_funcs.read import Fields
+except ImportError:
+    Fields = None
+
+#-------------------------------------------------
+
+DEFAULT_NUM_GRID = 32
+DEFAULT_NUM_DIM = 3
+
+#-------------------------------------------------
+
+def seed(num=None, verbose=False):
+    if num is not None:
+        if verbose:
+            print('setting numpy.random.seed=%d' % num)
+        np.random.seed(num)
+
+#-------------------------------------------------
+
+def load(fields, path=None, num_grid=DEFAULT_NUM_GRID, num_dim=DEFAULT_NUM_DIM, max_edgelength=None, verbose=False, Verbose=False):
+    """standardize logic for loading data and/or generating synthetic data
+    """
+    verbose |= Verbose
+    data = dict()
+
+    if path is not None: # read data from file
+        if verbose:
+            print('loading: '+path)
+
+        if Fields is None:
+            raise ImportError('could not import PLASMAtools.read_funcs.read.Fields')
+
+        turb = Fields(path, reformat=True)
+
+        # read the fields
+        for field in fields:
+            turb.read(field, verbose=Verbose)
+            data[field] = getattr(turb, field) # replacement for this syntax: turb.vel
+
+        del turb # get rid of this object to save memory
+
+    else: # generate random data on a big-ish 3D array
+
+        shape = (1,)+(num_grid,)*num_dim
+        if verbose:
+            print('generating randomized data with shape: %s' % (shape,))
+
+        # use grid to compute coherent structure
+        x = np.arange(num_grid) / num_grid
+        if num_dim > 1:
+            xs = np.meshgrid(*(x for x in range(num_dim)), indexing='ij')
+            coherent = 0.5*np.exp(-0.5*np.sum((xs[:-1]-xs[-1])**2)/0.1**2) ### a tube
+
+        else:
+            coherent = 0.5*np.exp(-0.5*(x-0.5)**2/0.1**2) ### a bump
+
+        # iterate through fields and add Gaussia noise
+        for field in fields:
+            data[field] = coherent + np.random.normal(size=shape)
+
+    #---
+
+    if verbose:
+        for field in fields:
+            print('    '+field, data[field].shape) # expect [num_dim, num_x, num_y, num_z]
+
+    #---
+
+    if max_edgelength is not None:
+        if verbose:
+            print('limiting data size by selecting the first max(edgelength)=%d samples' % max_edgelength)
+
+        for key in data.keys():
+            data[key] = data[key][:, :max_edgelength, :max_edgelength, :max_edgelength]
+
+    #---
+
+    return data
+
 #-------------------------------------------------
 
 def moments(samples, index):
@@ -46,7 +127,7 @@ def central_moments(samples, index):
             mom[ind] += m[k] * (-m[0])**(ind-k) * comb(ind, k)
 
     # compute covariance matrix
-    raise NotImplementedError
+    raise NotImplementedError('compute covariance matrix!')
 
     # return
     return index, mom, cov

67070d9668601777873d31955ab52e4402b656a3
Reed Essick <reed.essick@gmail.com>
2025-02-10 10:55:10 -0500

    misc work to get moment workflow up and running

--- env.sh
+++ env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-export PATH="$PWD/opt/bin:$PATH"
-
+# pick up PLASMAtools
 export PYTHONPATH="$PWD/opt:$PYTHONPATH"
 
+# pick up local installs (may live in different locations on different machines)
+export PATH="$PWD/opt/bin:$PATH"
 export PYTHONPATH="$PWD/opt/lib/python3.10/site-packages/:$PYTHONPATH"
-#export PYTHONPATH="$PWD/opt/local/lib/python3.10/dist-packages/:$PYTHONPATH"
+
+export PATH="$PWD/opt/local/bin:$PATH"
+export PYTHONPATH="$PWD/opt/local/lib/python3.10/dist-packages/:$PYTHONPATH"
